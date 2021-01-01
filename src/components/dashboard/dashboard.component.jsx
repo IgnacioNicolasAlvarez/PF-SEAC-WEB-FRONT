10677b0bf01b696b6c785fbeaeff791be4ff04fa
Ignacio Alvarez <ignacionicolasalvarez@gmail.com>
2021-01-01 15:17:27 -0300

    primer commit

---
+++ src/components/dashboard/dashboard.component.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+
+import './dashboard.styles.css'
+
+class Dashboard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      solicitudes: [],
+    };
+  }
+
+  componentWillMount() {
+    fetch("https://seac-backend.azurewebsites.net/solicitudes")
+      .then((Response) => Response.json())
+      .then((solicitudes) => this.setState({ solicitudes: solicitudes }));
+  }
+
+  renderTableHeader() {
+    return (
+      <tr>
+        <th>Id</th>
+        <th>Fecha</th>
+        <th>descripcion</th>
+        <th>location</th>
+        <th>estado</th>
+        <th>clasficacion</th>
+        <th>polaridad</th>
+      </tr>
+    );
+  }
+
+  renderTableData() {
+    return this.state.solicitudes.map((solicitud) => {
+      const {
+        id,
+        location,
+        fecha,
+        descripcion,
+        estado,
+        clasificacion,
+        sentimentalismo,
+      } = solicitud;
+      return (
+        <tr key={id}>
+          <td>{id}</td>
+          <td>{fecha}</td>
+          <td>{descripcion}</td>
+          <td>{location}</td>
+          <td>{estado}</td>
+          <td>{clasificacion}</td>
+          <td>{sentimentalismo}</td>
+          <td>
+            <button>Boton</button>
+          </td>
+        </tr>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 id="titulo_tabla">Solicitudes</h1>
+        <table id="solicitudes">
+          <tbody>
+            {this.renderTableHeader()}
+            {this.renderTableData()}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
---
+++ src/components/sidebar/sidebar.component.jsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+
+const Sidebar = (props) => {
+  return <div></div>;
+};
+export default Sidebar;
