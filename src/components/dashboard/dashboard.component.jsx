65d1f198334f61bdffd885c1c480ecb318b9c4ce
Ignacio Alvarez <ignacionicolasalvarez@gmail.com>
2021-01-01 21:03:48 -0300

    searchbox

--- src/components/dashboard/dashboard.component.jsx
+++ src/components/dashboard/dashboard.component.jsx
@@ -1,24 +1,22 @@
 import React, { Component } from "react";
-import Moment from 'moment';
+import Moment from "moment";
+import { Button } from "@material-ui/core";
 
-import './dashboard.styles.css'
+import { SearchBox } from "../seach-box/search-box.component";
+import "./dashboard.styles.css";
 
 class Dashboard extends Component {
   constructor() {
     super();
     this.state = {
       solicitudes: [],
+      filtro_descripcion: "",
     };
   }
 
-  verDetalle ()
-  {
+  verDetalle() {}
 
-  }
-
-
-
-  componentWillMount() {
+  componentDidMount() {
     fetch("https://seac-backend.azurewebsites.net/solicitudes")
       .then((Response) => Response.json())
       .then((solicitudes) => this.setState({ solicitudes: solicitudes }));
@@ -39,9 +37,9 @@ class Dashboard extends Component {
     );
   }
 
-  renderTableData() {
-    Moment.locale('ar');
-    return this.state.solicitudes.map((solicitud) => {
+  renderTableData(solicitudes_filtradas) {
+    Moment.locale("ar");
+    return solicitudes_filtradas.map((solicitud) => {
       const {
         id,
         location,
@@ -54,14 +52,20 @@ class Dashboard extends Component {
       return (
         <tr key={id}>
           <td>{id}</td>
-          <td>{Moment(fecha).format('L')}</td>
+          <td>{Moment(fecha).format("L")}</td>
           <td>{descripcion}</td>
           <td>{location}</td>
           <td>{estado}</td>
           <td>{clasificacion}</td>
           <td>{sentimentalismo}</td>
-          <td className='operacion_verDetalle'>
-            <button onClick={() => this.verDetalle(id)}>Ver Detalle</button>
+          <td className="operacion_verDetalle">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => this.verDetalle(id)}
+            >
+              Ver Detalle
+            </Button>
           </td>
         </tr>
       );
@@ -69,19 +73,31 @@ class Dashboard extends Component {
   }
 
   render() {
+    const { solicitudes, filtro_descripcion } = this.state;
+    const solicitudes_filtradas = solicitudes.filter((solicitud) =>
+      solicitud.descripcion
+        .toLowerCase()
+        .includes(filtro_descripcion.toLowerCase())
+    );
+
     return (
       <div>
         <h1 id="titulo_tabla">Solicitudes</h1>
+        <SearchBox
+          placeholder="Buscar Descripcion"
+          handleChange={(e) => this.setState({ filtro_descripcion: e.target.value })}
+        ></SearchBox>
+
+        
         <table id="solicitudes">
           <tbody>
             {this.renderTableHeader()}
-            {this.renderTableData()}
-          </tbody>
+            {this.renderTableData(solicitudes_filtradas)}
+          </tbody> 
         </table>
       </div>
     );
   }
-
 }
 
 export default Dashboard;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,17 +1,47 @@
 import React, { Component } from "react";
-import { AppBar, Toolbar, IconButton } from "@material-ui/core";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  Container,
+} from "@material-ui/core";
 import { Home } from "@material-ui/icons";
+import "./header.styles.css";
 
-
+const navLinks = [
+  { title: `1`, path: `/1` },
+  { title: `2`, path: `/2` },
+  { title: `3`, path: `/3` },
+  { title: `4`, path: `/4` },
+  { title: `5`, path: `/5` },
+];
 
 class Header extends Component {
   render() {
     return (
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="home">
-            <Home fontSize="large" />
-          </IconButton>
+          <Container className="navbarDisplayFlex">
+            <List
+              className="navDisplayFlex"
+              component="nav"
+              aria-labelledby="main navigation"
+            >
+              <IconButton edge="start" color="inherit" aria-label="home">
+                <Home fontSize="inherit" />
+              </IconButton>
+              {navLinks.map(({ title, path }) => (
+                <a className="linkText" href={path} key={title}>
+                  <ListItem button>
+                    <ListItemText primary={title} />
+                  </ListItem>
+                </a>
+              ))}
+            </List>
+          </Container>
         </Toolbar>
       </AppBar>
     );
